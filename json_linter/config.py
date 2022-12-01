5e78d48f708e6976617128e7519ab59923a766d0
Christopher Kaster <me@atomicptr.de>
2022-12-01 16:17:43 +0100

    add enum for naming style conventions (unused)

--- json_linter/config.py
+++ json_linter/config.py
@@ -1,10 +1,17 @@
 """ Linter configuration """
 from dataclasses import dataclass
+from enum import Enum
 from typing import Optional
+
+NamingStyle = Enum("NamingStyle", [
+    "camel-case",
+    "snake-case",
+    "kebab-case",
+])
 
 
 @dataclass
 class LinterConfig:
     """ Configuration for the linter/fixer """
     indent: int
-    naming_style: Optional[str]
+    naming_style: Optional[NamingStyle]
